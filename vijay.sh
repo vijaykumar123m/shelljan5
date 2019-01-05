282758236331e469050f00d67ddfd14417e0db25
vijaykumar123m <45665928+vijaykumar123m@users.noreply.github.com>
2019-01-05 22:00:52 +0530

    Update vijay.sh

--- vijay.sh
+++ vijay.sh
@@ -1,9 +1,9 @@
  tar xzvf java.tar.gz
  export JAVA_HOME=jdk-install-dir
- export PATH=$JAVA_HOME/bin:$PATH
+ export PATH=$JAVA_HOME/bin:C:\Program Files\Java\jdk1.8.0_131\bin
  cd opt
  tar xzvf apache-maven-3.6.0-bin.tar.gz
- export PATH=/opt/apache-maven-3.6.0/bin:$PATH
+ export PATH=/opt/apache-maven-3.6.0/bin:D:\DEVOPS SOFT.W\DevOps Softwares\apache-maven-3.6.0-bin\apache-maven-3.6.0\bin
  curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins repo
  sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
  sudo yum install jenkins
