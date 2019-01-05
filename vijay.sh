5b6d30b98b2006aea6f494991524633879cd04d6
vijaykumar123m <45665928+vijaykumar123m@users.noreply.github.com>
2019-01-05 21:33:09 +0530

    Add files via upload

---
+++ vijay.sh
@@ -0,0 +1,16 @@
+ cat vijay.sh
+ tar xzvf java.tar.gz
+ export JAVA_HOME=jdk-install-dir
+ export PATH=$JAVA_HOME/bin:$PATH
+ cd opt
+ tar xzvf apache-maven-3.6.0-bin.tar.gz
+ export PATH=/opt/apache-maven-3.6.0/bin:$PATH
+ curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins repo
+ sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+ sudo yum install jenkins
+mkdir task1
+cd task1
+touch t1 t2 t3
+mkdir task2
+cp t1 task2
+
